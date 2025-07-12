61ca5ab71a2542d1085a22206b916d017a467fa4
Shivang Jha <shivangjha7@gmail.com>
2025-07-13 01:42:43 +0530

    Add real-time location sharing feature using Geolocation API and Socket.IO

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,27 @@
-io();
+const socket = io();
+socket.on("message", (message) => {
+  console.log(message);
+});
+
+const messagebox = document.getElementById("message");
+const sendButton = document.getElementById("submit");
+const updatedMessage = document.getElementById("updatedMessage");
+const kahahai = document.getElementById("location");
+sendButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("Click", messagebox.value);
+  socket.emit("sendMessage", messagebox.value);
+});
+
+document.querySelector("#sendLocation").addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser.");
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit("sendLocation", {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    });
+  });
+});
--- src/index.js
+++ src/index.js
@@ -3,15 +3,34 @@ const app = express();
 const path = require("path");
 const http = require("http");
 const socketio = require("socket.io");
+
 const server = http.createServer(app);
 const io = socketio(server);
 
 const viewsPath = path.join(__dirname, "../public");
-
-io.on("connection", () => {
-  console.log("New WebSocket connection");
-});
 app.use(express.static(viewsPath));
+
+io.on("connection", (socket) => {
+  console.log("New WebSocket connection");
+
+  socket.emit("message", "welcome");
+  socket.broadcast.emit("message", "new user connected");
+  socket.on("sendMessage", (message) => {
+    io.emit("message", message);
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", "a user disconnected");
+  });
+
+  socket.on("sendLocation", (coords) => {
+    io.emit(
+      "message",
+      `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+    );
+  });
+});
+
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log("Server is running on port ");
